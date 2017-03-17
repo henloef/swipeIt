7cf6df0cc2f89f3360a06ad8d9fc352e28e038cc
Sigrid <sigbrats@gmail.com>
2017-03-17 10:25:22 +0100

    add pictures and cardmodel extends Sprite

--- core/src/com/swipeit/game/Models/CardModel.java
+++ core/src/com/swipeit/game/Models/CardModel.java
@@ -1,19 +1,26 @@
 package com.swipeit.game.Models;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 /**
  * Created by Lars on 10.03.2017.
  */
 
-public class CardModel {
+public class CardModel extends Sprite{
     private Direction direction;
 
     public Direction getDirection() {
         return direction;
     }
 
-    public CardModel(Direction direction){
+    public CardModel(Texture texture, Direction direction){
+        this.setTexture(texture);
         this.direction = direction;
+
     }
+
+
     //
 
     /*
