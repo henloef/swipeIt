1eb624feea86ea3e8dbdc5b343e02326174d5c8d
Sigrid <sigbrats@gmail.com>
2017-03-17 13:03:41 +0100

    added cam and width and height

--- core/src/com/swipeit/game/SwipeIt.java
+++ core/src/com/swipeit/game/SwipeIt.java
@@ -5,10 +5,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.swipeit.game.Models.CardModel;
+import com.swipeit.game.Models.Direction;
 
 public class SwipeIt extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	public static final float w = Gdx.graphics.getWidth();
+	public static final float h = Gdx.graphics.getHeight();
 	
 	@Override
 	public void create () {
@@ -21,7 +25,7 @@ public class SwipeIt extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
