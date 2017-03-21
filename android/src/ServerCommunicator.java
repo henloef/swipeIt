31455ebad50a4ae33a85fddefc606ecf0b14efd2
mariessa <marie.sagedal@hotmail.com>
2017-03-21 15:03:17 +0100

    move ServerCommunicator to Android package, because Core is a jar

---
+++ android/src/ServerCommunicator.java
@@ -0,0 +1,10 @@
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by marie on 19.03.17.
+ */
+
+public class ServerCommunicator {
+
+    private FirebaseDatabase mFireDatatabase;
+}
